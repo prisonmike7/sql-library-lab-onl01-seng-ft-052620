39881e89d5ffd70b778bfe2e0863e8dffc88599f
Michael Harris <mwharris7@gmail.com>
2020-06-30 07:41:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ def select_name_and_motto_of_char_with_longest_motto
   <<-SQL
     SELECT characters.name, characters.motto
     FROM characters
-    ORDER BY LEN(characters.motto) DESC
+    ORDER BY LENGTH(characters.motto) DESC
     LIMIT 1;
   SQL
 end
