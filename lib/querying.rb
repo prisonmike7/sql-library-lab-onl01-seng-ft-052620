fcb270c4c81cb5afc6d7ad2e44c76bccb4d4edab
Michael Harris <mwharris7@gmail.com>
2020-06-30 07:52:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,19 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  <<-SQL
+    SELECT series.title FROM series
+    JOIN books
+    ON books.series_id = series.id
+    JOIN character_books
+    ON character_books.book_id = books.id
+    JOIN characters
+    ON characters.id = character_books.character_id
+    WHERE characters.species = "human"
+    GROUP BY series.title
+    ORDER BY COUNT(characters.id) DESC
+    LIMIT 1;
+  SQL
 end
 
 def select_character_names_and_number_of_books_they_are_in
